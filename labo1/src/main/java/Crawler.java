1cee9e02d80454b8cac753c59ec3e8e3757ccb19
Lewis Jaggi <lewisjaggi@gmail.com>
2020-02-28 11:43:46 +0100

    labo1 avec import crawler et fichier pom (sdk11)

---
+++ labo1/src/main/java/Crawler.java
@@ -0,0 +1,7 @@
+import edu.uci.ics.crawler4j.crawler.WebCrawler;
+public class Crawler extends WebCrawler {
+
+    public static void main(String[] args) {
+        System.out.println("test import");
+    }
+}
