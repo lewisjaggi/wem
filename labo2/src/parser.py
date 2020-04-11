3a7f67d508bc73c4bce08a3dd754d1ebbe6f3541
Dylan Netuschil <dnetuschil@2cs.ch>
2020-04-11 21:47:29 +0200

    finish parser

--- labo2/src/parser.py
+++ labo2/src/parser.py
@@ -1,31 +1,50 @@
-import pandas as pd
 import numpy as np
+import csv
 
-df_a = pd.read_csv("../data/superstore_dataset2011-2015.csv",
-                   sep=",",
-                   encoding="latin1",
-                   header=0,
-                   usecols=["Customer ID", "Product ID"])
+data = []
 
 
-products = df_a["Product ID"].copy().drop_duplicates()
-nbr_product = products.size
-print(nbr_product)
-header = pd.concat([pd.Series(["Customer ID"]), products]).array
-# print(header)
-
-data_out = pd.DataFrame(columns=header).set_index("Customer ID")
+def get_column_id(name, h):
+    return np.where(np.array(h) == name)[0]
 
 
-all_false = ["Toto"]
-for x in range(0, nbr_product):
-    all_false.append("False")
-
-print("AllFalseSize = " + str(len(all_false)))
-
-new_row = pd.Series(all_false)
-print(new_row)
-data_out = pd.concat([data_out, new_row], ignore_index=True)
+def create_empty_row(customer_name, nbr_product):
+    array_empty = [customer_name]
+    for x in range(0, nbr_product):
+        array_empty.append("False")
+    return np.array([array_empty])
 
 
-print(data_out)
+with open("../data/superstore_dataset2011-2015.csv", newline="") as csvFile:
+    reader = csv.reader(csvFile, delimiter=',')
+    header = next(reader, None)
+    print("Header : [ " + " | ".join(header) + " ]")
+    print(reader)
+    for row in reader:
+        data.append(row)
+data = np.array(data)
+interestingData = data[:, [get_column_id("Customer Name", header), get_column_id("Product Name", header)]]
+
+customers = np.unique(interestingData[:, 0])
+products = np.unique(interestingData[:, 1])
+
+header_out = np.concatenate([["Customer Name"], products])
+
+data_out = np.array([header_out])
+i = 0
+for row in interestingData:
+    i = i + 1
+    customerName = str(row[0][0])
+    productName = str(row[1][0])
+    print(str(i) + " / " + str(len(interestingData)))
+
+    if customerName not in data_out:
+        emptyRow = create_empty_row(customerName, len(products))
+        data_out = np.append(data_out, emptyRow, axis=0)
+
+    rn = np.where(data_out == customerName)[0]
+    cn = np.where(header_out == productName)[0]
+    data_out[rn[0], cn[0]] = True
+
+np.savetxt("foo.csv", data_out, delimiter=",")
+print("END")
